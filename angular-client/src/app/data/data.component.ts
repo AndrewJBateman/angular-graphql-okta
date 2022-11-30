851ca2b950e7a4b9d9516a14b0371951914afb3b
AndrewJBateman <gomezbateman@yahoo.com>
2022-11-30 10:48:13 +0100

    chore: frontend ng mat dependency update to v15

--- angular-client/src/app/data/data.component.ts
+++ angular-client/src/app/data/data.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { MatPaginator } from '@angular/material/paginator';
+import { MatLegacyPaginator as MatPaginator } from '@angular/material/legacy-paginator';
 import { MatSort } from '@angular/material/sort';
-import { MatTableDataSource } from '@angular/material/table';
+import { MatLegacyTableDataSource as MatTableDataSource } from '@angular/material/legacy-table';
 import { Apollo } from 'apollo-angular';
 import { PARTS_LIST } from 'src/app/graphql/graphql.queries';
 import { Part } from './models/part.model';
